8317a39b6ca8527db215e288ee1e8a874db58e1e
Andrea Panceri <pancer.apps@gmail.com>
2023-08-10 17:50:06 +0200

    db create

--- flask-server/server.py
+++ flask-server/server.py
@@ -9,14 +9,18 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/projectL'
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
+
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(100), nullable=False)
-    #created_at = db.Column(db.DateTime, nullable=False, defalut=datetime.utcnow)
+    # created_at = db.Column(db.DateTime, nullable=False, defalut=datetime.utcnow)
 
     def __repr__(self):
-        return  f"Event: {self.description}"
-    
+        return f"Event: {self.description}"
+
     def __init__(self, description):
         self.description = description
 
@@ -25,9 +29,10 @@ class Event(db.Model):
 
 @app.route("/members")
 def members():
-    return {"members": ["Member1","Member2","Member3"]}
+    return {"members": ["Member1", "Member2", "Member3"]}
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
-
