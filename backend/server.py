d70051f8b2833c74d88d28a6842f67b75258b326
Andrea Panceri <pancer.apps@gmail.com>
2023-08-20 00:53:14 +0200

    Changes

--- backend/server.py
+++ backend/server.py
@@ -13,12 +13,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db.init_app(app)
 app.register_blueprint(event_bp, url_prefix='/events')
 
+
 @app.route("/", methods=['GET'])
 def home():
-    return {"Backend Server is up"}
+    return "Backend Server is up"
+
 
 if __name__ == "__main__":
-    #with app.app_context():
-        #db.create_all()
+    with app.app_context():
+        db.create_all()
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
